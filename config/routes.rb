bd9b3966655767775d56b300e1b9b073c7dfb6d1
Pedro Paulo Paiva de Medeiros <pdr_mdrs@hotmail.com>
2016-06-21 02:05:53 -0300

    Controller and view added
    Added the Demo controller and view;
    Added default route;
    Added db password;

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'demo/index'
+  match ':controller(/:action(/:id))', :via => :get
+  # root "demo#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
